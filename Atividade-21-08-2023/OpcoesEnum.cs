b0103e22f8fb1380c3e10f65c250476cc4a6df9d
ca-rib <108152033+ca-rib@users.noreply.github.com>
2023-10-13 14:54:47 -0300

    Create OpcoesEnum.cs

---
+++ Atividade-21-08-2023/OpcoesEnum.cs
@@ -0,0 +1,19 @@
+//Caroline e Vinnicius 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade-21-08-2023
+{
+    public enum OpcoesEnum
+    {
+        Sair = 0,
+        Adicionar = 1,
+        Pesquisar = 2,
+        Alterar = 3,
+        Remover = 4,
+        Listar = 5
+    }
+}
