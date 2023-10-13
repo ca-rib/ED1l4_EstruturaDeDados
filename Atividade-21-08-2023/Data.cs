f1c4f54bc5bf7dfa693d2a7a02a27518d3f0b199
ca-rib <108152033+ca-rib@users.noreply.github.com>
2023-10-13 14:53:55 -0300

    Create Data.cs

---
+++ Atividade-21-08-2023/Data.cs
@@ -0,0 +1,38 @@
+//Caroline e Vinnicius 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade-21-08-2023
+{
+    public class Data
+    {
+
+        public int Dia { get; set; }
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+
+        public Data()
+            : this(DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year)
+        {
+        }
+
+        public Data(int dia, int mes, int ano)
+        {
+            Dia = dia;
+            Mes = mes;
+            Ano = ano;
+        }
+
+        public void setData(int dia, int mes, int ano)
+        {
+            Dia = dia;
+            Mes = mes;
+            Ano = ano;
+        }
+
+        public override string ToString() => $"{Dia:00}/{Mes:00}/{Ano:0000}";
+    }
+}
