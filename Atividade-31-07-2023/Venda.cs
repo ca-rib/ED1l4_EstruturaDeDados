598e79816107221d2b4f7f8a10b2c86de9b5dff1
ca-rib <108152033+ca-rib@users.noreply.github.com>
2023-10-13 14:37:40 -0300

    Create Venda.cs

---
+++ Atividade-31-07-2023/Venda.cs
@@ -0,0 +1,36 @@
+//Caroline e Vinnicius 
+
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade-31-07-2023
+{
+    public class Venda
+    {
+        private int qtde;
+        private double valor;
+
+        public int Qtde { get => qtde; set => qtde = value; }
+        public double Valor { get => valor; set => valor = value; }
+
+        public Venda(int qtde, double valor)
+        {
+            this.qtde = qtde;
+            this.valor = valor;
+        }
+
+        public Venda() : this(0, 0)
+        {   
+        }
+
+        public double valorMedio() => this.valor / this.qtde;
+
+        public override string ToString()
+        {
+            return $"Valor: {this.Valor:C} - Qtde: {this.Qtde}";
+        }
+    }
+}
